655286b76d0a235b157cfed44bc39a6541ed796b
marinanicolai <nicolaimarina@gmail.com>
2023-08-16 21:37:03 +0500

    Added user_id state

--- src/components/AppContext.js
+++ src/components/AppContext.js
@@ -11,13 +11,14 @@ export function AppContextProvider({ children }) {
    const [document_user_id, setDocumentUserId] = useState();
    const [dailyDosageCount, setDailyDosageCount] = useState();
    const [dailyOccurrence, setDailyOccurrence] = useState([]);
+   const [user_id,setUserId]= useState("");
   // ... other states
 
   return (
     <AppContext.Provider
       value={{
         name,setName,age,setAge,dob,setDob,medicineName,setMedicineName,startDate,setStartDate,document_user_id,setDocumentUserId,
-        dailyDosageCount,setDailyDosageCount,dailyOccurrence,setDailyOccurrence
+        dailyDosageCount,setDailyDosageCount,dailyOccurrence,setDailyOccurrence,user_id,setUserId
         // ... other state and setter pairs
       }}
     >
